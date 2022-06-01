2e61e13420f0db7524b038e6f287bc1467ea77bb
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-01 18:59:55 +0530

    Added to PermutationsOfString

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("\t\t\t\t\tWelcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine("\n 1. Program to return all permutations of a string");
+                
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        PermutationsOfString permute = new PermutationsOfString();
+                        permute.Driver();
+                        break;
+                    
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
         }
     }
 }
